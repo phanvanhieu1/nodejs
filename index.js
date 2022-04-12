815e02e9ac07b608c5e3b804c40476c907c41879
phanvanhieu1 <phanvanhieu12a2@gmail.com>
2022-04-12 09:59:19 +0700

    .

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/api',(req,res)=>{
     });
 
 
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log('listening on port ' + PORT);
 });
